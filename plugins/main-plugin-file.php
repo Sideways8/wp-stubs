154e17ab94d079299cdb5890192a1445ef4227b1
Marty Eason <martye@sideways8.com>
2018-06-03 10:26:34 -0400

    Add main plugin file stub

---
+++ plugins/main-plugin-file.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: My Plugin
+Plugin URI:
+Description:
+Author:
+Version:
+Author URI:
+*/
+
+/** Absolute path to plugin directory (with trailing slash). */
+define( 'MY_PLUGIN_DIR', trailingslashit( __DIR__ ) );
+
+/** Public URL to plugin directory (with trailing slash). */
+define( 'MY_PLUGIN_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
