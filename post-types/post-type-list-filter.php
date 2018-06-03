929265a4e5fb63288115cef8d5eefd453caef57d
Marty Eason <martye@sideways8.com>
2018-06-03 16:04:03 -0400

    Correct hook

--- post-types/post-type-list-filter.php
+++ post-types/post-type-list-filter.php
@@ -5,7 +5,7 @@ class CustomPostTypeListFilter
         $post_types = ['post'];
         foreach( $post_types as $post_type ) {
             add_filter( "manage_{$post_type}_posts_columns", [$this, 'columns'] );
-            add_filter( "manage_{$post_type}_sortable_columns", [$this, 'sortable_columns'], 10, 2 );
+            add_filter( "manage_edit-{$post_type}_sortable_columns", [$this, 'sortable_columns'] );
             add_action( "manage_{$post_type}_manage_posts_custom_column", [$this, 'column_content'], 10, 2 );
         }
     }
