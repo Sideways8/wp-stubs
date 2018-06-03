682d2d96fb548ba31c32875748f60fee6e8437c7
Marty Eason <martye@sideways8.com>
2018-06-03 15:58:09 -0400

    Add custom post type list filter

---
+++ post-types/post-type-list-filter.php
@@ -0,0 +1,29 @@
+<?php
+class CustomPostTypeListFilter
+{
+    public function __construct() {
+        $post_types = ['post'];
+        foreach( $post_types as $post_type ) {
+            add_filter( "manage_{$post_type}_posts_columns", [$this, 'columns'] );
+            add_filter( "manage_{$post_type}_sortable_columns", [$this, 'sortable_columns'], 10, 2 );
+            add_action( "manage_{$post_type}_manage_posts_custom_column", [$this, 'column_content'], 10, 2 );
+        }
+    }
+
+    public function columns( $columns ) {
+        $columns['_custom_column'] = 'Custom Column';
+        return $columns;
+    }
+
+    public function sortable_columns( $columns ) {
+        return $columns;
+    }
+
+    public function column_content( $column_name, $post_id ) {
+        switch( $column_name ) {
+            case '_custom_column':
+                // Do something...
+                break;
+        }
+    }
+}
