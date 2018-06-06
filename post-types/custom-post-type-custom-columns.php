906405200ae29aad2bffae06030e3d411de93fd9
zawntech <martye@sideways8.com>
2018-06-06 17:49:43 -0400

    Correct hook

--- post-types/custom-post-type-custom-columns.php
+++ post-types/custom-post-type-custom-columns.php
@@ -6,7 +6,7 @@ class CustomPostTypeListFilter
         foreach( $post_types as $post_type ) {
             add_filter( "manage_{$post_type}_posts_columns", [$this, 'columns'] );
             add_filter( "manage_edit-{$post_type}_sortable_columns", [$this, 'sortable_columns'] );
-            add_action( "manage_{$post_type}_manage_posts_custom_column", [$this, 'column_content'], 10, 2 );
+            add_action( "manage_{$post_type}_posts_custom_column", [$this, 'column_content'], 10, 2 );
         }
     }
 
